3f61c1e01f977ac056ad92857157b26576c6a8c5
kouhei <kouhei.basketball7.8@gmail.com>
2024-06-23 21:05:52 +0900

    fix:typescriptに変更したため、拡張子変更と少し修正

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  const [ characterInfo, setCharacterInfo ] = useState();
+  
+  return (
+    <div className="App">
+      HelloNarutoWorld
+    </div>
+  );
+}
+
+export default App;
