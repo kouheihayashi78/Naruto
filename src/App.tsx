5e75625180bbf5699fb8563d78fe3b5fd83706e4
kouhei <kouhei.basketball7.8@gmail.com>
2024-06-23 21:15:35 +0900

    add:NARUTO APIからキャラクターの情報をaxiosを用いて非同期取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
   const [ characterInfo, setCharacterInfo ] = useState();
-  
+  useEffect(() => {
+    fetchCharacters(); 
+  },[]);
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+    const res = await axios.get(apiUrl);
+    console.log(res)
+  }
   return (
     <div className="App">
       HelloNarutoWorld
