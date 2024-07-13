d40c6ca7a697e407d5c45fbea92e1cbdd3167f3d
kouhei <kouhei.basketball7.8@gmail.com>
2024-07-13 16:34:39 +0900

    add:ナルトのキャラクターの情報を画面に表示する実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [ characterInfo, setCharacterInfo ] = useState();
+  const [characterInfo, setCharacterInfo] = useState([]);
   useEffect(() => {
-    fetchCharacters(); 
-  },[]);
+    fetchCharacters();
+  }, []);
   const fetchCharacters = async () => {
     const apiUrl = "https://narutodb.xyz/api/character";
     const res = await axios.get(apiUrl);
-    console.log(res)
-  }
+    setCharacterInfo(res.data.characters);
+  };
   return (
-    <div className="App">
-      HelloNarutoWorld
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characterInfo.map((character) => {
+            return (
+              <div className="card" key={character.id}>
+                <img src={character.images[0] ?? "/dummy.png"} 
+                  alt="character"
+                  className="card-image"
+                  />
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">{character.debut?.appearsIn ?? 'なし'}</p>
+                  <div className="card-footer">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? 'なし'}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
