f97eb84491855a7860fc06b1698c9586cde11179
kouhei <kouhei.basketball7.8@gmail.com>
2024-07-13 17:11:31 +0900

    fix:index.htmlの型エラーを修正

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 明示的に型アサーションを行い、nullになる可能性を除外する
+const rootElement = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
