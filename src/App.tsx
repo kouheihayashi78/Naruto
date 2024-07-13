6a74ed29f7ea4b6ebce5316e892177555e8c1c39
kouhei <kouhei.basketball7.8@gmail.com>
2024-07-13 17:10:38 +0900

    add:ローダーを回す処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import "./App.css";
 function App() {
   const [characterInfo, setCharacterInfo] = useState([]);
   const [pagerNum, setPagerNum] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     fetchCharacters(pagerNum);
   }, [pagerNum]);
   const fetchCharacters = async (page) => {
+    setIsLoading(true);
     const apiUrl = "https://narutodb.xyz/api/character";
     const res = await axios.get(apiUrl, { params: {page} });
     setCharacterInfo(res.data.characters);
+    setIsLoading(false);
   };
   const nextPager = () => {
     setPagerNum(parseInt(pagerNum) + 1)
@@ -22,34 +25,36 @@ function App() {
   };
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {characterInfo.map((character) => {
-            return (
-              <div className="card" key={character.id}>
-                <img src={character.images[0] ?? "/dummy.png"} 
-                  alt="character"
-                  className="card-image"
-                  />
-                <div className="card-content">
-                  <h3 className="card-title">{character.name}</h3>
-                  <p className="card-description">{character.debut?.appearsIn ?? 'なし'}</p>
-                  <div className="card-footer">
-                    <span className="affiliation">
-                      {character.personal?.affiliation ?? 'なし'}
-                    </span>
+      {isLoading ? <h3>Now Loading...</h3> :
+        <main>
+          <div className="cards-container">
+            {characterInfo.map((character) => {
+              return (
+                <div className="card" key={character.id}>
+                  <img src={character.images[0] ?? "/dummy.png"} 
+                    alt="character"
+                    className="card-image"
+                    />
+                  <div className="card-content">
+                    <h3 className="card-title">{character.name}</h3>
+                    <p className="card-description">{character.debut?.appearsIn ?? 'なし'}</p>
+                    <div className="card-footer">
+                      <span className="affiliation">
+                        {character.personal?.affiliation ?? 'なし'}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="pager">
-          <button className="prev" onClick={prevPager}>Previous</button>
-          <span className="page-number">{pagerNum}</span>
-          <button className="next" onClick={nextPager}>Next</button>
-        </div>
-      </main>
+              );
+            })}
+          </div>
+          <div className="pager">
+            <button className="prev" onClick={prevPager}>Previous</button>
+            <span className="page-number">{pagerNum}</span>
+            <button className="next" onClick={nextPager}>Next</button>
+          </div>
+        </main>
+      }
     </div>
   );
 }
